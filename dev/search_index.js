39caf11ea57b3009e0462b523aff37fa73bb468f
Documenter.jl <documenter@juliadocs.github.io>
2025-06-28 18:15:17 +0000

    build based on 8493325

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#EBMPackage","page":"Home","title":"EBMPackage","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EBMPackage.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"#EBMPackage.add_two-Tuple{T} where T<:Number","page":"Home","title":"EBMPackage.add_two","text":"add_two(x::T) where {T<:Number}\n\nAdds two to the input number.\n\n\n\n\n\n","category":"method"}]
+}
